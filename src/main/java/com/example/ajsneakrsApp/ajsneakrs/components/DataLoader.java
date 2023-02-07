9788b5a85dfd1392ad1954a50988c3739560c90b
alejandrojg <alejandrojg8@gmail.com>
2023-02-07 10:43:59 +0000

    loaded new items

--- src/main/java/com/example/ajsneakrsApp/ajsneakrs/components/DataLoader.java
+++ src/main/java/com/example/ajsneakrsApp/ajsneakrs/components/DataLoader.java
@@ -28,16 +28,17 @@ public class DataLoader {
         Manufacturer manufacturer1 = new Manufacturer();
         manufacturer1.setProducts(new ArrayList<>());
         manufacturer1.setName("Nike");
-        manufacturer1.setStatus(true);
         manufacturerRepository.save(manufacturer1);
 
         Manufacturer manufacturer2 = new Manufacturer();
         manufacturer2.setProducts(new ArrayList<>());
         manufacturer2.setName("Adidas");
-        manufacturer2.setStatus(true);
         manufacturerRepository.save(manufacturer2);
 
-
+        Manufacturer manufacturer3 = new Manufacturer();
+        manufacturer3.setProducts(new ArrayList<>());
+        manufacturer3.setName("New Balance");
+        manufacturerRepository.save(manufacturer3);
 
         Product product1 = new Product();
         product1.setName("Air Jordan 1");
@@ -49,64 +50,78 @@ public class DataLoader {
         product1.setSellPrice(190);
         productRepository.save(product1);
 
+
         Product product2 = new Product();
-        product2.setName("Adidas Forum");
-        product2.setDescription("Bad Bunny White");
-        product2.setSize(7.5);
-        product2.setManufacturer(manufacturer2);
+        product2.setName("Air Jordan 3");
+        product2.setDescription("Remastered 2023");
+        product2.setSize(7);
+        product2.setManufacturer(manufacturer1);
         product2.setStockQty(2);
-        product2.setBuyPrice(140);
-        product2.setSellPrice(170);
+        product2.setBuyPrice(200);
+        product2.setSellPrice(320);
         productRepository.save(product2);
 
         Product product3 = new Product();
-        product3.setName("Adidas Forum");
-        product3.setDescription("Bad Bunny Black");
-        product3.setSize(7);
-        product3.setManufacturer(manufacturer2);
-        product3.setStockQty(4);
-        product3.setBuyPrice(140);
-        product3.setSellPrice(190);
+        product3.setName("Air Jordan 1");
+        product3.setDescription("Obsidian");
+        product3.setSize(6);
+        product3.setManufacturer(manufacturer1);
+        product3.setStockQty(3);
+        product3.setBuyPrice(170);
+        product3.setSellPrice(280);
         productRepository.save(product3);
 
         Product product4 = new Product();
-        product4.setName("Air Jordan 2");
-        product4.setDescription("Union LA Blue");
+        product4.setName("Air Force 1");
+        product4.setDescription("Triple White");
         product4.setSize(8);
         product4.setManufacturer(manufacturer1);
         product4.setStockQty(2);
-        product4.setBuyPrice(140);
+        product4.setBuyPrice(120);
         product4.setSellPrice(190);
         productRepository.save(product4);
 
         Product product5 = new Product();
-        product5.setName("Air Jordan 3");
-        product5.setDescription("Reimagined White Cement");
-        product5.setSize(6);
+        product5.setName("Nike Cortez");
+        product5.setDescription("University Blue");
+        product5.setSize(7.5);
         product5.setManufacturer(manufacturer1);
-        product5.setStockQty(5);
-        product5.setBuyPrice(120);
-        product5.setSellPrice(200);
+        product5.setStockQty(6);
+        product5.setBuyPrice(100);
+        product5.setSellPrice(120);
         productRepository.save(product5);
 
         Product product6 = new Product();
-        product6.setName("Air Jordan 4");
-        product6.setDescription("Oreo");
-        product6.setSize(9);
+        product6.setName("Air Jordan 11");
+        product6.setDescription("Concord");
+        product6.setSize(11);
         product6.setManufacturer(manufacturer1);
-        product6.setStockQty(3);
-        product6.setBuyPrice(170);
-        product6.setSellPrice(300);
+        product6.setStockQty(5);
+        product6.setBuyPrice(150);
+        product6.setSellPrice(190);
         productRepository.save(product6);
 
+
         Product product7 = new Product();
-        product7.setName("Adidas Yeezy");
-        product7.setDescription("350 V2 Zebra");
-        product7.setSize(7.5);
+        product7.setName("Adidas Ultraboost");
+        product7.setDescription("Triple Black 22");
+        product7.setSize(7);
         product7.setManufacturer(manufacturer2);
-        product7.setStockQty(4);
-        product7.setBuyPrice(200);
-        product7.setSellPrice(260);
+        product7.setStockQty(5);
+        product7.setBuyPrice(150);
+        product7.setSellPrice(190);
         productRepository.save(product7);
+
+        Product product8 = new Product();
+        product8.setName("New Balance 990V3");
+        product8.setDescription("Teddy Santis Marblehead");
+        product8.setSize(7);
+        product8.setManufacturer(manufacturer3);
+        product8.setStockQty(1);
+        product8.setBuyPrice(200);
+        product8.setSellPrice(240);
+        productRepository.save(product8);
+
+
     }
 }
